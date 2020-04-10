89042ca0fe7cb5e8fd9757b4389231650cde9290
Your Name <francesco.cognolato@gmail.com>
2020-04-10 15:01:49 +1000

    Fix

--- src/utils.py
+++ src/utils.py
@@ -46,14 +46,7 @@ def getDriver(path):
 
 	# Profile
 	profile = webdriver.FirefoxProfile()
-	'''headers = {
-		'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-		'referrer': 'https://google.com',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-		'Accept-Encoding': 'gzip, deflate, br',
-		'Accept-Language': 'en-US,en;q=0.9',
-		'Pragma': 'no-cache',
-	}'''
+	
 	# Write headers
 	headers = {
 		"user-agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
